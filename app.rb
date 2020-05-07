e27db91c996385d038a3e8bec078e96f3c9a8b73
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-07 04:28:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -20,6 +20,10 @@ class App < Sinatra::Base
 
   get '/set' do
     # set the :foo key of the session hash equal to 'hello' here!
+
+session["foo"] = "hello"
+ @session = session
+
     if session[:foo] == 'hello'
       redirect '/fetch'
     else
@@ -36,26 +40,27 @@ class App < Sinatra::Base
   end
 
   get '/set_session' do
-    #set session id here
+      session[:id] = 1
+      #set session id here
+      if session[:id] == 1
+        # "Session ID set. It's currently set to #{session[:user_id]}."
+        redirect '/fetch_session_id'
+      else
+        "Session ID has not been set!"
+      end
+    end
 
-    if session[:id] == 1
-      # "Session ID set. It's currently set to #{session[:id]}."
-      redirect '/fetch_session_id'
-    else
-      "Session ID has not been set!"
+    get '/fetch_session_id' do
+      "You did it! session[:id] value: #{session[:id]}.\nNow, clear the session in the '/logout' route.\nSee the readme for further instructions.\nThen, navigate to the '/logout' path."
+    end
+
+    get '/logout' do
+      session = {}
+
+      "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
+    end
+
+    get '/finish' do
+      "Hopefully that explains a little more about the concept of sessions.\nThe session is simply a way to store user data on a temporary basis.\nIn any web application, a user ID is typically used as a session ID.\nThis is because an ID attribute of a user is a unique identifier\nthat will always be distinguishable from other user ID attributes."
     end
   end
-
-  get '/fetch_session_id' do
-    "You did it! session[:id] value: #{session[:id]}.\nNow, clear the session in the '/logout' route.\nSee the readme for further instructions.\nThen, navigate to the '/logout' path."
-  end
-
-  get '/logout' do
-    #clear session hash here
-    "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
-  end
-
-  get '/finish' do
-    "Hopefully that explains a little more about the concept of sessions.\nThe session is simply a way to store user data on a temporary basis.\nIn any web application, a user ID is typically used as a session ID.\nThis is because an ID attribute of a user is a unique identifier\nthat will always be distinguishable from other user ID attributes."
-  end
-end
