1b58c74cdc339e5412aa9e64cc7b1ed832c75b16
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-07 04:26:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -49,17 +49,17 @@ session["foo"] = "hello"
         "Session ID has not been set!"
       end
     end
-  
+
     get '/fetch_session_id' do
       "You did it! session[:user_id] value: #{session[:user_id]}.\nNow, clear the session in the '/logout' route.\nSee the readme for further instructions.\nThen, navigate to the '/logout' path."
     end
-  
+
     get '/logout' do
       session = {}
-  
+
       "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
     end
-  
+
     get '/finish' do
       "Hopefully that explains a little more about the concept of sessions.\nThe session is simply a way to store user data on a temporary basis.\nIn any web application, a user ID is typically used as a session ID.\nThis is because an ID attribute of a user is a unique identifier\nthat will always be distinguishable from other user ID attributes."
     end
